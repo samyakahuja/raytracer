f017a6ac486f3b926f8a6aacba00929e83d2c4d8
samyakahuja <samyak.mcs18.du@gmail.com>
2020-06-24 04:26:05 +0530

    add basic ray from ch 4

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,4 +1,5 @@
 use raytracer::vec3::Vec3;
+use raytracer::ray::Ray;
 
 fn write_color(color: Vec3) {
     let color = 255.999 * color;
@@ -10,20 +11,41 @@ fn write_color(color: Vec3) {
     );
 }
 
+fn ray_color(ray: &Ray) -> Vec3 {
+    let unit_direction = Vec3::unit_vector(ray.direction);
+    // 0.0 <= t <= 1.0
+    let t = 0.5 * (unit_direction.y + 1.0);
+    // linear interpolation between white and some bluish color
+    (1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
+}
+
 fn main() {
-    const WIDTH: i32 = 256;
-    const HEIGHT: i32 = 256;
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const WIDTH: i32 = 384;
+    const HEIGHT: i32 = (WIDTH as f64 / ASPECT_RATIO as f64) as i32;
 
     // PP3 file header
     println!("P3\n{} {}\n255", WIDTH, HEIGHT);
 
+    // viewport has same aspect ratio as the image.
+    let viewport_height: f64 = 2.0;
+    let viewport_width: f64 = ASPECT_RATIO * viewport_height;
+    // distance between the projection point and the projection plane
+    let focal_length: f64 = 1.0;
+
+    let origin = Vec3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+
     for j in (0..HEIGHT).rev() {
         eprintln!("\rScanlines remaining: {}", j);
         for i in 0..WIDTH {
-            let r = i as f64 / (WIDTH - 1) as f64;
-            let g = j as f64 / (HEIGHT - 1) as f64;
-            let b = 0.25;
-            let col = Vec3::new(r, g, b);
+            let u = i as f64 / (WIDTH - 1) as f64;
+            let v = j as f64 / (HEIGHT - 1) as f64;
+            // start traversing at lower_left_corner
+            let ray = Ray::new(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            let col = ray_color(&ray);
             write_color(col);
         }
     }
---
+++ src/ray.rs
@@ -0,0 +1,29 @@
+use crate::vec3::Vec3;
+
+pub struct Ray {
+    pub origin: Vec3,
+    pub direction: Vec3,
+}
+
+impl Ray {
+    pub fn new(origin: Vec3, direction: Vec3) -> Self {
+        Ray { origin, direction }
+    }
+
+    pub fn point_at_parameter(&self, t: f64) -> Vec3 {
+        self.origin + t * self.direction
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn point_in_ray() {
+        let ray = Ray::new(Vec3::new(1.0, 2.0, 3.0), Vec3::new(1.0, 2.0, 3.0));
+        let t = 2.0;
+
+        assert_eq!(ray.point_at_parameter(t), Vec3::new(3.0, 6.0, 9.0));
+    }
+}
